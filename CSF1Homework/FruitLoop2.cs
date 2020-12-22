a332fae3e891af49bee23342cc4501dd4e0136a6
bslenker <bslenker@gmail.com>
2020-12-22 12:19:23 -0600

    FruitLoop2 completed

--- CSF1Homework/FruitLoop2.cs
+++ CSF1Homework/FruitLoop2.cs
@@ -11,6 +11,21 @@ namespace CSF1Homework
         static void Main(string[] args)
         {
             Console.WriteLine("Fruit Loop #2\n");
+
+            //array built out of 6 fruits declared as a string(text)
+            string[] fruits = { "Pomegranate", "Pear", "Peach", "Paw Paw", "Pinapple", "Papaya", };
+            //array built for the dots
+            string[] dots = { ".", "..", "...", "....", ".....", "......" };
+
+
+            //for loop
+            for (int timesRan = 0; timesRan < fruits.Length; timesRan++)// timesRan is # of times ran through a loop, fruits.length = the lenth of the array
+            {
+
+                string loopingFruit = fruits[timesRan];
+                Console.WriteLine("Fruits starting with a P: {0},\n{1} ", fruits[timesRan], dots[timesRan]);//outputing fruits through the loop {0}, with a return inbetween dots output.
+                
+            }//end for
         }//end Main()
     }//end class
 }//end namespace
