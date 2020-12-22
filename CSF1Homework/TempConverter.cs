52565251f787fd733116ac7f340edc573f15ac7d
bslenker <bslenker@gmail.com>
2020-12-22 15:30:51 -0600

    merged MadLib to master  and completed temp converter class

--- CSF1Homework/TempConverter.cs
+++ CSF1Homework/TempConverter.cs
@@ -11,6 +11,59 @@ namespace CSF1Homework
         static void Main(string[] args)
         {
             Console.WriteLine("Temperature Converter\n");
+
+            //Console.Write("Please enter the temperature in Celsius: ");
+            //double celC = double.Parse(Console.ReadLine());
+            //double fahC = celC * (9.0 / 5.0) + 32;
+            //Console.WriteLine("The temperature is: " + fahC + "\u00B0F." + "\n");
+
+            //Console.Write("Please enter the temperature in Fahrenheit: ");
+            //double fahF = double.Parse(Console.ReadLine());
+            //double celF = (fahF - 32) / (9.0 / 5.0);
+            //Console.WriteLine("The temperature is: " + celF + "\u00B0C." + "\n");
+            
+            
+            
+
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("Please select Celsius or Fahrenheit converter:");
+                Console.WriteLine("C) Celsius\n" +
+                    "F) Fahrenheit\n" +
+                    "X) Exit\n");
+                string userChoice = Console.ReadLine().ToUpper();
+
+                Console.Clear();
+
+                switch(userChoice)
+                {
+                    case "C":
+                    case "Celsius":
+                        Console.Write("Please enter the temperature in Celsius: ");
+                        double celC = double.Parse(Console.ReadLine());
+                        double fahC = celC * (9.0 / 5.0) + 32;
+                        Console.WriteLine("The temperature is: " + fahC + "\u00B0F." + "\n");
+                        break;
+                    case "F":
+                    case "FAHRENHEIT":
+                        Console.Write("Please enter the temperature in Fahrenheit: ");
+                        double fahF = double.Parse(Console.ReadLine());
+                        double celF = (fahF - 32) / (9.0 / 5.0);
+                        Console.WriteLine("The temperature is: " + celF + "\u00B0C." + "\n");
+                        break;
+                    case "X":
+                    case "EXIT":
+                    case "E":
+                        Console.WriteLine("Thank you for using our temperature converter! Have nice day!");
+                        repeat = false;
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid option, please try again...\n");
+                        break;
+                }//end switch
+
+            } while (repeat);
         }//end Main()
     }//end class
 }//end namespace
