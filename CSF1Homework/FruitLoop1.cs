6fa423187f782319c25eb6107388d934d9bdf011
bslenker <bslenker@gmail.com>
2020-12-22 11:39:57 -0600

    FruitLoop1 class completed

--- CSF1Homework/FruitLoop1.cs
+++ CSF1Homework/FruitLoop1.cs
@@ -11,6 +11,17 @@ namespace CSF1Homework
         static void Main(string[] args)
         {
             Console.WriteLine("Fruit Loop #1\n");
+
+            //array built out of 6 fruits declared as a string(text)
+            string[] fruits = { "Pomegranate", "Pear", "Peach", "Paw Paw", "Pinapple", "Papaya" };
+
+            //for loop
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                string loopingFruit = fruits[i];
+                Console.WriteLine("6 fruit names are: " + loopingFruit );
+            }//end for
+
         }//end Main()
     }//end class
 }//end namespace
