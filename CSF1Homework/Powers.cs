1652289a2cc5eef67cbfefa92a1e57f9b62858a9
bslenker <bslenker@gmail.com>
2021-01-04 09:42:07 -0600

    First Challenge completed.

---
+++ CSF1Homework/Powers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Powers Lab");
+
+            /*
+             * Using a FOR loop, display the powers of 2 from 1 to 50.
+             * (2 to the 1st power through 2 to the 50th power). Use a long variable to store your results.
+             * Ex: 2 to the 4th power is 2x2x2x2.
+             */
+             
+
+            for (double i = 1; i <= 50; i++)
+            {
+                
+                long answer = (long)Math.Pow(i,2);
+               
+                Console.WriteLine(answer);
+                
+            }//end for
+
+        }//end main
+    }//end class
+}//end namespace
